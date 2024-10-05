d8d417abb33d952b9e4e59f3adcc0275ea794d08
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2024-10-05 16:28:22 +0700

    feaT: config deployment

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -62,13 +62,12 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, result }) => {
     const handleDownloadPDF = async (file: ChatFile) => {
         setIsMakingPDF(true);
         try {
-            const apiUrl = 'https://c43119d5-c421-49ff-a2db-5864a86a3749.railway.app/generate-pdf';
-
-            const response = await fetch(apiUrl, {
+            const response = await fetch('https://c43119d5-c421-49ff-a2db-5864a86a3749.railway.app/generate-pdf', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ htmlContent: file.content, name: file.filename }),
+                body: JSON.stringify({ htmlContent: file.content, name: file.filename })
             });
+
             if (!response.ok) {
                 throw new Error('Failed to generate PDF');
             }
